7af4d4f780c4f7d29bd7de98f37461980010a585
JDUNK <jdunk4me@gmail.com>
2026-03-27 00:17:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,8 @@ const path = require("path");
 const http = require("http");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("MML Arcade Server is running!");
-});
-
 const server = http.createServer(app);
-
-const wss = new WebSocketServer({ 
-  server,
-  perMessageDeflate: false
-});
+const wss = new WebSocketServer({ server });
 
 const arcadePath = path.join(__dirname, "arcade.html");
 
@@ -25,29 +16,20 @@ const mmlDocument = new EditableNetworkedDOM(
   () => fs.readFileSync(arcadePath, "utf8")
 );
 
-wss.on("connection", (ws, req) => {
-  console.log("Client connected from:", req.headers.origin || "unknown");
+wss.on("connection", (ws) => {
+  console.log("Client connected");
   mmlDocument.addWebSocket(ws);
-
   ws.on("close", () => {
     console.log("Client disconnected");
     mmlDocument.removeWebSocket(ws);
   });
-
-  ws.on("error", (err) => {
-    console.error("WebSocket error:", err.message);
-  });
 });
 
-// Handle upgrade explicitly
-server.on("upgrade", (request, socket, head) => {
-  console.log("WebSocket upgrade request received");
-  wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit("connection", ws, request);
-  });
+app.get("/", (req, res) => {
+  res.send("MML Arcade Server is running!");
 });
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, () => {
   console.log(`MML server running on port ${PORT}`);
 });
