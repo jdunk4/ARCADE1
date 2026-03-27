923b5a7c26a012692f4479e3b6f2bbc1237a13aa
JDUNK <jdunk4me@gmail.com>
2026-03-26 23:32:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,32 +2,38 @@ const express = require("express");
 const { WebSocketServer } = require("ws");
 const { EditableNetworkedDOM } = require("@mml-io/networked-dom-server");
 const fs = require("fs");
+const path = require("path");
 const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-// Load the MML document
+// Use path.join to find arcade.html relative to this file
+const arcadePath = path.join(__dirname, "arcade.html");
+const arcadeContent = fs.readFileSync(arcadePath, "utf8");
+
 const mmlDocument = new EditableNetworkedDOM(
   "http://localhost/arcade.html",
-  (htmlPath) => fs.readFileSync("./arcade.html", "utf8")
+  () => arcadeContent
 );
 
 // Handle WebSocket connections
 wss.on("connection", (ws) => {
+  console.log("Client connected");
   mmlDocument.addWebSocket(ws);
   ws.on("close", () => {
+    console.log("Client disconnected");
     mmlDocument.removeWebSocket(ws);
   });
 });
 
-// Health check endpoint
+// Health check
 app.get("/", (req, res) => {
   res.send("MML Arcade Server is running!");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`MML server running on port ${PORT}`);
 });
